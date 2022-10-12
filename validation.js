b4434d30f1ebdfabc86273461bcbf1b0e2e48600
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-10-12 19:36:25 +0600

    validation means type of declared variable or object or array checked

---
+++ validation.js
@@ -0,0 +1,12 @@
+const number = 23;
+if(typeof number === 'number'){
+    console.log('value is a number');
+}
+else{
+    console.log('value is not a number');
+}
+
+const numbers = [2, 3, 4, 5, 6];
+const student = {name: 'rongila rz'}
+console.log(Array.isArray(numbers));
+console.log(typeof student);
